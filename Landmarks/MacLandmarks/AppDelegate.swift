18b71e18aa60635cc2c7237e9924abdf21da7874
sogafish <k.sogafish@gmail.com>
2020-08-01 19:51:58 +0900

    Combine the Primary and Detail Views for macOS

--- Landmarks/MacLandmarks/AppDelegate.swift
+++ Landmarks/MacLandmarks/AppDelegate.swift
@@ -11,13 +11,10 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     var window: NSWindow!
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let contentView = ContentView().environmentObject(UserData())
 
         // Create the window and set the content view. 
         window = NSWindow(
--- Landmarks/MacLandmarks/ContentView.swift
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -2,22 +2,30 @@
 //  ContentView.swift
 //  MacLandmarks
 //
-//  Created by k.soga on 2020/07/26.
+//  Created by k.soga on 2020/08/01.
 //  Copyright © 2020 Apple. All rights reserved.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedLandmark: Landmark?
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationView {
+            NavigationPrimary(selectedLandmark: $selectedLandmark)
+            
+            if selectedLandmark != nil {
+                NavigationDetail(landmark: selectedLandmark!)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(UserData())
     }
 }
